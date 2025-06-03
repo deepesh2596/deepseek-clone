3329ce8f6c30898bdd4165a576120744cf7c83de
deepesh <deepesh2596@gmail.com>
2025-06-03 10:52:04 +0530

    initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,9 @@ import { assets } from "@/assets/assets";
 import Messages from "@/components/Messages";
 import PromptBox from "@/components/PromptBox";
 import Sidebar from "@/components/Sidebar";
+import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 
 export default function Home() {
@@ -12,11 +13,29 @@ export default function Home() {
   const [expand, setExpand] = useState(false)
   const [messages, setMessages] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const { selectedChat } = useAppContext()
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages)
+    }
+  }, [selectedChat])
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behaviour: "smooth",
+      })
+    }
+  }, [messages])
+
 
   return (
     <div>
       <div className="flex h-screen">
-        <Sidebar expand={expand} setExpand={setExpand}/>
+        <Sidebar expand={expand} setExpand={setExpand} />
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image onClick={() => (expand ? setExpand(false) : setExpand(true))}
@@ -35,13 +54,28 @@ export default function Home() {
           ) :
 
             (
-              <div>
-                <Messages role='user' content='What is NextJs'/>
+              <div ref={containerRef} className="relative flex flex-col items-center justify-start w-full mt-20 max-h-screen overflow-y-auto">
+                <p className="fixed top-8 border border-transparent hover:border-gray-500/50 py-1 px-2 rounded-lg font-semibold mb-6">{selectedChat.name}</p>
+                {messages.map((msg, index) => (
+                  <Messages key={index} role={msg.role} content={msg.content} />
+                ))}
+                {
+                  isLoading && (
+                    <div className="flex gap-4 max-w-3xl w-full py-3">
+                      <Image className="h-9 w-9 p-1 bordeer border-white/15 rounded-full" src={assets.logo_icon} alt=""/>
+                      <div className="loader flex justify-center items-center gap-1">
+                        <div className="w-1 h-1 roundeed-full bg-white animate-bounce"></div>
+                        <div className="w-1 h-1 roundeed-full bg-white animate-bounce"></div>
+                        <div className="w-1 h-1 roundeed-full bg-white animate-bounce"></div>
+                      </div>
+                    </div>
+                  )
+                }
               </div>
             )
           }
 
-         <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
+          <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
           <p className="text-xs absolute bottom-1 text-gray-500">AI-generated, for reference only</p>
 
         </div>
